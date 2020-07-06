7a95b7cbf379607a9c321ff1551e835fec30119a
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-07-06 15:06:14 +0100

    conda rhdf5 does not help

--- workflow/scripts/renv.R
+++ workflow/scripts/renv.R
@@ -1,1 +1,10 @@
+# issue installing Rhdf5lib from source
+# renv supports configure args: https://rstudio.github.io/renv/reference/install.html
+# Could that be the solution?
+# installation of RNetCDF may require us to set include paths for netcdf
+# configure.args = c(RNetCDF = "--with-netcdf-include=/usr/include/udunits2"))
+# options(configure.args = configure.args)
+
+options(Ncpus = 2) # TODO: parameterize
+
 renv::init()
